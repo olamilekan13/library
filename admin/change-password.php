97adf2c8e7545c0516c736cf1fecda2a084aaa5f
olamilekan <olamilekanomosanya@gmail.com>
2021-11-24 18:26:18 +0100

    add admin database

--- admin/change-password.php
+++ admin/change-password.php
@@ -34,7 +34,7 @@ $error="Your current password is wrong";
 
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
